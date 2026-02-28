33ec2715d56bfbd188a2e66caf6c5cdc1e4511ec
Nilarnab Debnath <nilarnabdebnath@gmail.com>
2026-02-27 20:48:03 -0500

    some more chnges

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,18 +1,32 @@
 const mongoose = require("mongoose");
 
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI missing");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-        process.env.MONGO_URI,{
-        serverSelectionTimeoutMS: 10000,
-        socketTimeoutMS: 45000,
-        }
-    );
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1);
-  }
-};
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  };
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = connectDB;
+
+
+
+
